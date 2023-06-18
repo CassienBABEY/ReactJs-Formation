d2f95993d9ff0f4ae76c5d3cc71014ce97ed743e
CassienBABEY <cassienbabey@gmail.com>
2023-06-18 05:04:34 +0200

    Props

--- POKEMON-APP/src/App.tsx
+++ POKEMON-APP/src/App.tsx
@@ -1,15 +1,9 @@
-import React, { FunctionComponent, useState } from 'react';
-import Pokemon from './models/pokemon';
-import POKEMONS from './models/mock-pokemon'
+import React, { FunctionComponent, useState, useEffect } from 'react';
+import PokemonList from './pages/pokemon-list';
 
 const App: FunctionComponent = () => {
-  const [pokemons] = useState<Pokemon[]>(POKEMONS);
-
   return (
-    <div>
-      <h1>Pokedex</h1>
-      <p>Il y a {pokemons.length} pokemons dans le Pokedex !</p>
-    </div>
+    <PokemonList />
   )
 }
 
