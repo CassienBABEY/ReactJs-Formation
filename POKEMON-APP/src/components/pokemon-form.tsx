ff63c26ea3a4bb12dda8de0b4984a87608e0b01d
CassienBABEY <cassienbabey@gmail.com>
2023-06-29 04:58:01 +0200

    .'

--- POKEMON-APP/src/components/pokemon-form.tsx
+++ POKEMON-APP/src/components/pokemon-form.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Pokemon from '../models/pokemon';
 import formatType from '../helpers/format-type';
+import PokemonService from '../services/pokemon-service';
 
   
 type Props = {
@@ -71,9 +72,13 @@ const PokemonForm: FunctionComponent<Props> = ({pokemon}) => {
     const isFormValid = validateForm();
 
     if(isFormValid) {
-      history.push(`/pokemons/${pokemon.id}`);
+        pokemon.name = form.name.value;
+        pokemon.hp = form.hp.value;
+        pokemon.cp = form.cp.value;
+        pokemon.types = form.types.value;
+        PokemonService.updatePokemon(pokemon).then(() => history.push(`/pokemons/${pokemon.id}`) );
+      }
     }
-  }
 
   const validateForm = () => {
     let newForm: Form = form;
